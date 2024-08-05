12befd2dcc02398e494bd4d56f2d4f17364106fd
0810746 <90589773+0810746@users.noreply.github.com>
2024-08-05 22:38:43 +0800

    Update and rename chunks_retrieval.py to test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+$ python
+Python 3.x.x (default, ... , 2024, ...) 
+[GCC x.x.x] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> text = """
+... apple,
+... is better.
+... """
+>>> print(text)
+apple,
+is better.
