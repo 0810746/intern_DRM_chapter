bd1788001b07a2e2930912e56b924e5fdeeda5fc
0810746 <90589773+0810746@users.noreply.github.com>
2024-08-07 22:39:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,25 @@
-$ python
-Python 3.x.x (default, ... , 2024, ...) 
-[GCC x.x.x] on linux
-Type "help", "copyright", "credits" or "license" for more information.
->>> text = """
-... apple,
-... is better.
-... """
->>> print(text)
-apple,
-is better.
+from bs4 import BeautifulSoup
+
+def extract_div_text(soup):
+    texts = []
+
+    def traverse(element):
+        if element.name == 'div':
+            for child in element.children:
+                traverse(child)
+        elif element.string:
+            texts.append(element.string.strip())
+        else:
+            # 如果element不是div且没有直接的文本內容，則遍歷其子元素
+            for child in element.children:
+                traverse(child)
+
+    traverse(soup)
+    return texts
+
+soup = BeautifulSoup(html, 'html.parser')
+texts = extract_div_text(soup)
+
+# 输出所有提取到的文本
+for text in texts:
+    print(text)
